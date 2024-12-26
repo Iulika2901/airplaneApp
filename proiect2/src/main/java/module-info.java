214a577409c0ea612d3fa4edafb18711a3bb1144
Bacriu_Iuliana <84437252+Iulika2901@users.noreply.github.com>
2024-12-26 20:00:08 +0200

    Delete proiect2 directory

--- proiect2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.proiect2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.proiect2 to javafx.fxml;
-    exports org.example.proiect2;
-}
